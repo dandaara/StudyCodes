5f6b2cb380dbac861e7d80d928554675f0980ad7
Dandara Aranha <dandaraaranha@gmail.com>
2016-03-30 11:58:58 -0300

    Pasta com Código
    testing

---
+++ TestCommit/src/Client.java
@@ -0,0 +1,32 @@
+
+
+public class Client {
+	
+	private int id;
+	private int address;
+	
+	public Client(int address){
+		this.address=(address);
+	}
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public int getAddress() {
+		return address;
+	}
+
+	public void setAddress(int a) {
+		this.address = a;
+	}
+
+	
+	
+	
+
+}
